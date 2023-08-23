5d87aeed3c8d6842ba909eeea674e5a5a215d744
Aleksandra Przygoda <alex4art@gmail.com>
2023-08-23 20:29:33 +0200

    js updated - removed global variable

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 {
-    const welcome = () => {
-        console.log("Welcome!");
-    };
-
-    const buttonElement = document.querySelector(".js-button");
-
     const toggleBackground = () => {
+        const buttonElement = document.querySelector(".js-button");
         const bodyElement = document.querySelector(".js-body");
         const themeNameElement = document.querySelector(".js-themeName");
 
         bodyElement.classList.toggle("darkBackgroundWrapper");
         buttonElement.classList.toggle("header__button--light");
+
         themeNameElement.innerText = bodyElement.classList.contains(
             "darkBackgroundWrapper"
         )
@@ -19,9 +15,9 @@
     };
 
     const init = () => {
-        buttonElement.addEventListener("click", toggleBackground);
+        const buttonElement = document.querySelector(".js-button");
 
-        welcome();
+        buttonElement.addEventListener("click", toggleBackground);
     };
 
     init();
