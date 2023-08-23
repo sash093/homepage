b918f82c4b91939d8cf53ebed0174ad1d89d0432
Aleksandra Przygoda <alex4art@gmail.com>
2023-08-23 14:32:37 +0200

    js refactoring-functions added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-console.log("Hello!");
+{
+    const welcome = () => {
+        console.log("Welcome!");
+    };
 
-let buttonElement = document.querySelector(".js-button");
-let bodyElement = document.querySelector(".js-body");
-let themeNameElement = document.querySelector(".js-themeName");
+    const buttonElement = document.querySelector(".js-button");
 
-buttonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("darkBackgroundWrapper");
-    buttonElement.classList.toggle("header__button--light");
-    themeNameElement.innerText = bodyElement.classList.contains(
-        "darkBackgroundWrapper"
-    )
-        ? "jasny"
-        : "ciemny";
-});
+    const toggleBackground = () => {
+        const bodyElement = document.querySelector(".js-body");
+        const themeNameElement = document.querySelector(".js-themeName");
+
+        bodyElement.classList.toggle("darkBackgroundWrapper");
+        buttonElement.classList.toggle("header__button--light");
+        themeNameElement.innerText = bodyElement.classList.contains(
+            "darkBackgroundWrapper"
+        )
+            ? "jasny"
+            : "ciemny";
+    };
+
+    const init = () => {
+        buttonElement.addEventListener("click", toggleBackground);
+
+        welcome();
+    };
+
+    init();
+}
